032c808323c68e082676626830422078f512ea2a
Muhamad Amirul <19381137+miesaf@users.noreply.github.com>
2020-09-20 18:44:01 +0800

    Create migration files

--- src/database/migrations/2020_09_20_102046_create_products_table.php
+++ src/database/migrations/2020_09_20_102046_create_products_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateProductsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,16 +13,16 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('products', function (Blueprint $table) {
             $table->id();
-            $table->string('username')->unique();
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->decimal('salesPrice', 8, 2);
+            $table->string('currencyCode');
+            $table->string('currencySymbol');
+            $table->integer('stockQty')->nullable();
             $table->boolean('isActive');
             $table->timestamps();
+            $table->bigInteger('merchant_id');
         });
     }
 
@@ -33,6 +33,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('products');
     }
 }
---
+++ src/database/migrations/2020_09_20_102205_create_transactions_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTransactionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->id();
+            $table->integer('qty');
+            $table->decimal('totalPrice', 8, 2);
+            $table->string('currencyCode');
+            $table->string('currencySymbol');
+            $table->timestamps();
+            $table->bigInteger('product_id');
+            $table->bigInteger('sale_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transactions');
+    }
+}

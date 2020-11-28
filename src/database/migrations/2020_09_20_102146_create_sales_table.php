12a4f7c5e67d4767f8a349cef9865fc4f1cdd5bf
Muhamad Amirul <19381137+miesaf@users.noreply.github.com>
2020-11-28 23:35:11 +0800

    Create migration files

---
+++ src/database/migrations/2020_09_20_102146_create_sales_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSalesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sales', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->decimal('cost', 8, 2);
+            $table->decimal('profit', 8, 2);
+            $table->string('currencyCode');
+            $table->string('currencySymbol');
+            $table->boolean('status');
+            $table->timestamps();
+            $table->bigInteger('merchant_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sales');
+    }
+}

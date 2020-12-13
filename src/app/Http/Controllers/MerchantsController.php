ad030c7e811906ff1896437e663b2c3af1500c38
Muhamad Amirul <19381137+miesaf@users.noreply.github.com>
2020-12-13 21:29:35 +0800

    Merchant: addProduct

--- src/app/Http/Controllers/MerchantsController.php
+++ src/app/Http/Controllers/MerchantsController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 use App\Merchant;
+use App\Product;
 use Str;
 
 class MerchantsController extends Controller
@@ -36,6 +37,36 @@ class MerchantsController extends Controller
             return response()->json(["code"=>200, "message"=>"Registered successfully."]);
         } else {
             return response()->json(["code"=>400, "message"=>"Registration failed."]);
+        }
+    }
+
+    public function addProduct(Request $request)
+    {
+        $request->validate([
+            'name' => ['required', 'string', 'max:255'],
+            'salesPrice' => ['required', 'numeric', 'max:999999.99'],
+            'currencyCode' => ['required', 'string', 'max:255'],
+            'currencySymbol' => ['required', 'string', 'max:255'],
+            'stockQty' => ['required', 'integer', 'max:99999999999'],
+            'merchant_id' => ['required', 'integer', 'max:99999999999999999999']
+        ]);
+
+        $product = new Product;
+
+        $product->name = $request['name'];
+        $product->salesPrice = $request['salesPrice'];
+        $product->currencyCode = $request['currencyCode'];
+        $product->currencySymbol = $request['currencySymbol'];
+        $product->stockQty = $request['stockQty'];
+        $product->isActive = 1;
+        $product->merchant_id = $request['merchant_id'];
+
+        $is_saved = $product->save();
+
+        if($is_saved){
+            return response()->json(["code"=>200, "message"=>"Product added successfully."]);
+        } else {
+            return response()->json(["code"=>400, "message"=>"Product addition failed."]);
         }
     }
 }
--- src/routes/api.php
+++ src/routes/api.php
@@ -19,4 +19,5 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 
 Route::group(['prefix' => 'merchant'], function(){
     Route::post('register', 'MerchantsController@register')->name('merchant.register');
+    Route::post('addProduct', 'MerchantsController@addProduct')->name('merchant.addProduct');
 });
